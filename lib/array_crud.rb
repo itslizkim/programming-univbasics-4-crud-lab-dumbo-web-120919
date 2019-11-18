9ef5c3d49c599ca368b11ac472441b8c6b8df0f3
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-11-18 03:28:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-["hey","hello","hi","bonjour"]
+ways_say_hello = ["hey","hello","hi","bonjour"]
 end
 
 def add_element_to_end_of_array(array, element)
